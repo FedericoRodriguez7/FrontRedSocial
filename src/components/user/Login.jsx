bab850ed0f801630aab5a2ad61077046a70e89cb
federico rodriguez <tafedericoemanuel@gmail.com>
2025-04-28 13:01:51 -0300

    navegacion

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <header className="content__header content__header--public">
+              <h1 className="content__title">Login</h1>
+              
+          </header>
+
+    <div className="content_posts">
+
+    </div>
+  </>
   )
 }
--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <header className="content__header content__header--public">
+                <h1 className="content__title">Registro</h1>
+                
+            </header>
+
+      <div className="content_posts">
+
+      </div>
+    </>
   )
 }
