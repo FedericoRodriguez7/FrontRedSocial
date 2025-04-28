d7dec24b767f1dd4b2dcc6859ca48ea8e0d4e0bc
federico rodriguez <tafedericoemanuel@gmail.com>
2025-04-28 13:43:29 -0300

    registro de usuarios

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -10,6 +10,36 @@ export const Register = () => {
 
       <div className="content_posts">
 
+          <form className='register-form'>
+
+                <div className='form-group'>
+                  <label htmlFor='name'>Nombre</label>
+                  <input type='text' name='name'></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='surname'>Apellido</label>
+                  <input type='text' name='apellido'></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='nick'>Nick</label>
+                  <input type='text' name='nick'></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='email'>Correo electronico</label>
+                  <input type='email' name='email'></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='password'>Contraseña</label>
+                  <input type='password' name='password'></input>
+                </div>
+
+                <input type='submit' value="Registrate" className='btn btn-success'></input>
+
+          </form>
       </div>
     </>
   )
