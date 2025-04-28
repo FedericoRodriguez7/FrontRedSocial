d1b76ffc2b333511a8974f5e8a2bbcdb755d4e4b
federico rodriguez <tafedericoemanuel@gmail.com>
2025-04-28 15:24:05 -0300

    hook de formulario

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
 
 export const Register = () => {
+
+  const { form, changed } = useForm({});
+
+  const saveUser = (e) => {
+    e.preventDefault();
+    
+    let newUser = form;
+    console.log(newUser);
+  }
+
+
+
   return (
     <>
       <header className="content__header content__header--public">
@@ -10,31 +23,31 @@ export const Register = () => {
 
       <div className="content_posts">
 
-          <form className='register-form'>
+          <form className='register-form' onSubmit={saveUser}>
 
                 <div className='form-group'>
                   <label htmlFor='name'>Nombre</label>
-                  <input type='text' name='name'></input>
+                  <input type='text' name='name' onChange={changed}></input>
                 </div>
 
                 <div className='form-group'>
                   <label htmlFor='surname'>Apellido</label>
-                  <input type='text' name='apellido'></input>
+                  <input type='text' name='apellido' onChange={changed}></input>
                 </div>
 
                 <div className='form-group'>
                   <label htmlFor='nick'>Nick</label>
-                  <input type='text' name='nick'></input>
+                  <input type='text' name='nick' onChange={changed}></input>
                 </div>
 
                 <div className='form-group'>
                   <label htmlFor='email'>Correo electronico</label>
-                  <input type='email' name='email'></input>
+                  <input type='email' name='email' onChange={changed}></input>
                 </div>
 
                 <div className='form-group'>
                   <label htmlFor='password'>Contraseña</label>
-                  <input type='password' name='password'></input>
+                  <input type='password' name='password' onChange={changed}></input>
                 </div>
 
                 <input type='submit' value="Registrate" className='btn btn-success'></input>
