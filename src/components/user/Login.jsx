8fbc34e67a924281d9f626e9db64c36484ee2ac3
federico rodriguez <tafedericoemanuel@gmail.com>
2025-05-05 17:45:00 -0300

    recoger datos de formulario

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
+import { Global } from '../helpers/Global';
 
 export const Login = () => {
+  
+
+  const { form, changed } = useForm({});
+  
+  const loginUser = async(e) => {
+      e.preventDefault();
+
+      let userToLogin = form;
+
+      //peticion al backend
+      const request = await fetch(Global.url+'user/login', {
+        method: "POST",
+        body: JSON.stringify(userToLogin),
+        headers: {
+          "Content-Type":"application/json"
+        }
+      })
+
+
+      const data = await request.json();
+      //persistir datos en el navegador
+
+      console.log(data)
+  }
+
+
+
   return (
     <>
     <header className="content__header content__header--public">
@@ -9,7 +38,20 @@ export const Login = () => {
           </header>
 
     <div className="content_posts">
+        <form className='form-login' onSubmit={loginUser}>
+          <div className='form-group'>
+            <label htmlFor="email">Email</label>
+            <input type="email" name='email' onChange={changed}/>
+          </div>
 
+          <div className='form-group'>
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" name='password' onChange={changed}/>
+          </div>
+
+          <input type="submit" value="Identificate" className='btn btn-success' />
+
+        </form>
     </div>
   </>
   )
