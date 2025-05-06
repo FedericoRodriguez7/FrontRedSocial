18c1ff433ca1c2b43da3a48ccaf04b42344f6167
federico rodriguez <tafedericoemanuel@gmail.com>
2025-05-06 17:52:17 -0300

    serializar form

--- src/components/user/Config.jsx
+++ src/components/user/Config.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useAuth from '../hooks/useAuth';
+import { Global } from '../helpers/Global';
+import { SerializeForm } from '../helpers/SerializeForm';
 
 export const Config = () => {
+
+    const {auth} = useAuth();
+    const [saved, setSaved] = useState("not-saved");
+
+    const updateUser = (e) => {
+        e.preventDefault();
+
+        //recoger datos del formulario
+      let newDataUser = SerializeForm(e.target)
+      delete newDataUser.file0;
+
+      //actualizar usuario en base de datos
+    }
+
   return (
-    <div>Config</div>
+    <>
+    <header className="content__header content__header--public">
+                <h1 className="content__title">Ajustes</h1>
+                
+            </header>
+
+            <div className='content__posts'>
+            {saved == "saved" ?
+        <strong className='alert alert-success'> Usuario registrado correctamente !! </strong>
+        : ''}
+
+        {saved == "error" ?
+        <strong className='alert alert-danger'> El usuario no se ha registrado !!</strong>
+        : ''}
+
+          <form className='config-form' onSubmit={updateUser}>
+
+                <div className='form-group'>
+                  <label htmlFor='name'>Nombre</label>
+                  <input type='text' name='name' defaultValue={auth.name}></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='surname'>Apellido</label>
+                  <input type='text' name='apellido' defaultValue={auth.surname}></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='nick'>Nick</label>
+                  <input type='text' name='nick' defaultValue={auth.nick}></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='bio'>Bio</label>
+                  <textarea name='bio' defaultValue={auth.bio}></textarea>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='email'>Correo electronico</label>
+                  <input type='email' name='email' defaultValue={auth.email}></input>
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='password'>Contraseña</label>
+                  <input type='password' name='password' ></input>
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor="file0">Avatar</label>
+                     {auth.image != "default.png" &&  <img src={Global.url + "user/avatar/" + auth.image} className="container-avatar__img" alt="Foto de perfil" />}
+                     {auth.image == "default.png" &&  <img src={avatar} className="container-avatar__img" alt="Foto de perfil" />}
+
+                              <br />                 
+                    <input type="file" name='file0' id='file' />
+
+                </div>
+                <br />
+                <input type='submit' value="Registrate" className='btn btn-success'></input>
+
+
+          </form>
+            </div>
+    
+    
+    </>
   )
 }
