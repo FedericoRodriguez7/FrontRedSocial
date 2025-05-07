08def7f4b605eb4f17963651832a9168c05f9743
federico rodriguez <tafedericoemanuel@gmail.com>
2025-05-07 19:54:51 -0300

    features

--- src/components/user/People.jsx
+++ src/components/user/People.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import avatar from "../../assets/img/user.png"
+import { Global } from '../helpers/Global'
 
 export const People = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  const getUsers = async() => {
+
+    //Peticion para sacar usuarios
+    const request = await fetch(Global.url + 'user/list/1', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem("token"),
+
+      }
+    });
+
+    const data = await request.json();
+
+    //crear un estado para poder listarlos
+    if(data.users && data.status == "success"){
+      setUsers(data.users);
+    }
+
+    //paginacion
+  }
 
 
   return (
@@ -13,25 +42,30 @@ export const People = () => {
 
             <div className="content__posts">
 
-                <article className="posts__post">
+                {users.map(user => {
+                  return (
+                    <article className="posts__post" key={user._id}>
 
                     <div className="post__container">
 
                         <div className="post__image-user">
                             <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
+                            {user.image != "default.png" &&  <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
+                            {user.image == "default.png" &&  <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
+                           
+                       
                             </a>
                         </div>
 
                         <div className="post__body">
 
                             <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
+                                <a href="#" className="user-info__name">{user.name} {user.surname}</a>
                                 <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
+                                <a href="#" className="user-info__create-date">{user.created_at}</a>
                             </div>
 
-                            <h4 className="post__content">Hola, buenos dias.</h4>
+                            <h4 className="post__content">{user.bio}</h4>
 
                         </div>
 
@@ -47,6 +81,10 @@ export const People = () => {
                     </div>
 
                 </article>
+                  )
+                  
+                })}
+                
 
                <div className='content__container-btn'>
                 <button className='content__btn-more-post'>
