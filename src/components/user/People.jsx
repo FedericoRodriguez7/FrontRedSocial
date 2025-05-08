b0428a5f1f0ed685843d29a9b5bb97be23652495
federico rodriguez <tafedericoemanuel@gmail.com>
2025-05-08 15:04:41 -0300

    ver mas usuarios

--- src/components/user/People.jsx
+++ src/components/user/People.jsx
@@ -5,15 +5,17 @@ import { Global } from '../helpers/Global'
 export const People = () => {
 
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [more, setMore] = useState(true);
 
   useEffect(() => {
-    getUsers();
+    getUsers(1);
   }, [])
 
-  const getUsers = async() => {
+  const getUsers = async (nextPage = 1) => {
 
     //Peticion para sacar usuarios
-    const request = await fetch(Global.url + 'user/list/1', {
+    const request = await fetch(Global.url + 'user/list/' + nextPage, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -25,77 +27,95 @@ export const People = () => {
     const data = await request.json();
 
     //crear un estado para poder listarlos
-    if(data.users && data.status == "success"){
-      setUsers(data.users);
+    if (data.users && data.status == "success") {
+
+      let newUsers = data.users;
+
+      if (users.length >= 1) {
+        newUsers = [...users, ...data.users];
+      }
+
+      setUsers(newUsers);
     }
 
     //paginacion
+
+    if(users.length >= (data.total - data.users.length)){
+      setMore(false);
+    }
+
+  }
+
+  const nextPage = () => {
+    let next = page + 1;
+    setPage(next);
+    getUsers(next)
   }
 
 
   return (
     <>
-         <header className="content__header">
-                <h1 className="content__title">Gente</h1>
-    
-            </header>
+      <header className="content__header">
+        <h1 className="content__title">Gente</h1>
 
-            <div className="content__posts">
+      </header>
 
-                {users.map(user => {
-                  return (
-                    <article className="posts__post" key={user._id}>
+      <div className="content__posts">
 
-                    <div className="post__container">
+        {users.map(user => {
+          return (
+            <article className="posts__post" key={user._id}>
 
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                            {user.image != "default.png" &&  <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
-                            {user.image == "default.png" &&  <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
-                           
-                       
-                            </a>
-                        </div>
+              <div className="post__container">
 
-                        <div className="post__body">
-
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">{user.name} {user.surname}</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">{user.created_at}</a>
-                            </div>
-
-                            <h4 className="post__content">{user.bio}</h4>
-
-                        </div>
-
-                    </div>
+                <div className="post__image-user">
+                  <a href="#" className="post__image-link">
+                    {user.image != "default.png" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
+                    {user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
 
 
-                    <div className="post__buttons">
+                  </a>
+                </div>
 
-                        <a href="#" className="post__button post__button--green">
-                            Seguir
-                        </a>
+                <div className="post__body">
 
-                    </div>
+                  <div className="post__user-info">
+                    <a href="#" className="user-info__name">{user.name} {user.surname}</a>
+                    <span className="user-info__divider"> | </span>
+                    <a href="#" className="user-info__create-date">{user.created_at}</a>
+                  </div>
 
-                </article>
-                  )
-                  
-                })}
-                
+                  <h4 className="post__content">{user.bio}</h4>
 
-               <div className='content__container-btn'>
-                <button className='content__btn-more-post'>
-                  Ver mas personas
-                </button>
-               </div>
-                
+                </div>
+
+              </div>
 
 
-            </div>
-    
+              <div className="post__buttons">
+
+                <a href="#" className="post__button post__button--green">
+                  Seguir
+                </a>
+
+              </div>
+
+            </article>
+          )
+
+        })}
+
+        {more &&
+          <div className='content__container-btn'>
+            <button className='content__btn-more-post' onClick={nextPage}>
+              Ver mas personas
+            </button>
+          </div>
+        }
+
+
+      </div>
+
     </>
   )
 }
